a769dd6d9fba7fa033ea05cf7864e749d7cc4b4b
Paweł Irzyk <pawel.irzyk6@gmail.com>
2022-04-19 12:18:37 +0200

    var name changed

--- src/rotrac_manager_node.py
+++ src/rotrac_manager_node.py
@@ -7,7 +7,7 @@ import rospy
 class RotracManager():
 
     def __init__(self):
-        self.load_params()
+        self._load_params()
         self.pub_cmd_vel_fl = rospy.Publisher('/rotrac_e2/front_left_wheel_velocity_controller/command',
                                             Float64,
                                             queue_size=1)
@@ -20,25 +20,25 @@ class RotracManager():
         self.pub_cmd_vel_rr = rospy.Publisher('/rotrac_e2/rear_right_wheel_velocity_controller/command',
                                            Float64,
                                            queue_size=1)
-        self.cmd_vel_sub = rospy.Subscriber('/cmd_vel', Twist, self.update_rotrac_velocity)
+        self.cmd_vel_sub = rospy.Subscriber('/cmd_vel', Twist, self._update_rotrac_velocity)
 
-    def load_params(self):
+    def _load_params(self) -> None:
         self.track_of_wheels = rospy.get_param("rotrac_manager/track_of_wheels")
         self.wheel_radius = rospy.get_param("rotrac_manager/wheel_radius")
 
-    def update_rotrac_velocity(self, msg: Twist) -> None:
+    def _update_rotrac_velocity(self, msg: Twist) -> None:
         self.calculate_wheels_veocity(msg.linear.x, msg.angular.z)
         self.publish_controller_commands()
 
     def calculate_wheels_veocity(self, V: float, w: float) -> None:
-        self.Vl = (2*V - w*self.track_of_wheels) / (2*self.wheel_radius)                                    
-        self.Vr = (2*V + w*self.track_of_wheels) / (2*self.wheel_radius)   
+        self.wheel_speed_left = (2*V - w*self.track_of_wheels) / (2*self.wheel_radius)                                    
+        self.wheel_speed_right = (2*V + w*self.track_of_wheels) / (2*self.wheel_radius)   
 
-    def publish_controller_commands(self):
-        self.pub_cmd_vel_fl.publish(self.Vl)
-        self.pub_cmd_vel_rl.publish(self.Vl)
-        self.pub_cmd_vel_fr.publish(self.Vr)
-        self.pub_cmd_vel_rr.publish(self.Vr)    
+    def publish_controller_commands(self) -> None:
+        self.pub_cmd_vel_fl.publish(self.wheel_speed_left)
+        self.pub_cmd_vel_rl.publish(self.wheel_speed_left)
+        self.pub_cmd_vel_fr.publish(self.wheel_speed_right)
+        self.pub_cmd_vel_rr.publish(self.wheel_speed_right)    
 
 if __name__=="__main__":
     rospy.init_node('rotrac_manager')
